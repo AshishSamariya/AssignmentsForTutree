997ece0f96c2609ccc69b7436de81fd132a3dd81
AshishSamariya <72882045+AshishSamariya@users.noreply.github.com>
2020-10-14 22:24:03 +0530

    Add files via upload

---
+++ InternshalaAssigmnent.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+int main(){
+	void incOrder(int);
+	incOrder(-7);
+	
+}
+
+/*void incOrder(int lastDigit){
+	
+	int count=1;
+	while(count<=lastDigit){
+		for(int i=count;i<=lastDigit;i++){
+			cout<<i<<" ";
+		}
+		cout<<endl;
+		for(int i=0;i<count;i++){
+			cout<<" ";
+		}
+		count++;
+	}
+	cout<<'\r';
+	
+	while(count>1){
+		
+		for(int i=count-2;i>0;i--){
+			cout<<" ";
+		}
+		for(int i =count-1;i<=lastDigit;i++){
+			cout<<i<<" ";
+		}
+		cout<<endl;
+		
+		
+		count--;
+	}
+}
+*/
+
+void incOrder(int lastDigit){
+	
+	int count=1;
+	while(count<=lastDigit){
+		for(int i=abs(count);i<=lastDigit;i++){
+			cout<<i<<" ";
+		}
+		cout<<endl;
+		for(int i=0;i<count;i++){
+			cout<<" ";
+		}
+		count++;
+	}
+	cout<<'\r';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ InternshalaAssigmnent2.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string str1= "XYYZXZYZXXYZ";
+	string str2= "XYZ";
+	
+	for(int i =0;i<(str1.size());i++) {
+		int j;
+		int count=0;
+		for(j=0;j<str2.size();j++){	
+			for(int k=0;k<str2.size();k++){
+				if((str2[j]==str1[i+k])){
+					count++;
+					break;
+				} 
+			}				
+		}	
+		if(j==str2.size() && count==str2.size()){
+			cout<<"Substring '";
+			for(int k =0;k<str2.size();k++){
+			cout<<str1[i+k];
+			}
+			cout<<"' present at index "<<i<<endl;
+		}
+	}
+	
+	/*for(int i =0;i<(str1.size())-2;i++) {
+		if((str2[0]==str1[i]) || (str2[0]==str1[i+1]) || (str2[0]==str1[i+2]) ){
+			if((str2[1]==str1[i]) || (str2[1]==str1[i+1]) || (str2[1]==str1[i+2]) ){
+				if((str2[2]==str1[i]) || (str2[2]==str1[i+1]) || (str2[2]==str1[i+2]) ){
+					cout<<"Substring "<<str1[i]<<str1[i+1]<<str1[i+2]<<" at index "<<i<<endl;
+				}
+			}
+		}
+	}*/
+	
+	/*for(int i =0;i<(str1.size())-2;i++) {
+		int j;
+		for(j=0;j<str2.size();j++){
+			if((str2[j]==str1[i]) || (str2[j]==str1[i+1]) || (str2[j]==str1[i+2]) ){
+				
+			}else{
+				break;
+			}				
+		}
+		if(j==str2.size()){
+			cout<<"Substring "<<str1[i]<<str1[i+1]<<str1[i+2]<<" at index "<<i<<endl;
+		}
+	}*/
+		
+}
