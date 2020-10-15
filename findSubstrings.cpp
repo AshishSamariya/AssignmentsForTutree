eb1c0234ef7746ed790daf1fa89f3d3e032fb044
AshishSamariya <72882045+AshishSamariya@users.noreply.github.com>
2020-10-15 15:23:49 +0800

    Update findSubstrings.cpp

--- findSubstrings.cpp
+++ findSubstrings.cpp
@@ -22,21 +22,5 @@ int main(){
 			}
 			cout<<"' present at index "<<i<<endl;
 		}
-	}
-	
-	//Solution for the specific problem
-	/*for(int i =0;i<(str1.size())-2;i++) {
-		int j;
-		for(j=0;j<str2.size();j++){
-			if((str2[j]==str1[i]) || (str2[j]==str1[i+1]) || (str2[j]==str1[i+2]) ){
-				
-			}else{
-				break;
-			}				
-		}
-		if(j==str2.size()){
-			cout<<"Substring "<<str1[i]<<str1[i+1]<<str1[i+2]<<" at index "<<i<<endl;
-		}
-	}*/
-		
+	}		
 }
